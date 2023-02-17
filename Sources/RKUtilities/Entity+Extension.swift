ea7c02da0cdda1f9573d13a93cbe7f967f9b13b0
Grant Jarvis <54558641+Reality-Dev@users.noreply.github.com>
2023-02-17 01:39:05 -0600

    Expose license further.

--- Sources/RKUtilities/Entity+Extension.swift
+++ Sources/RKUtilities/Entity+Extension.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE file and the LICENSE ORIGINS folder for this sample’s licensing information.
  */
 
 import RealityKit
